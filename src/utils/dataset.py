0c04d4958a86abba2dd2ea3dbdd645b88196018e
eric2.monzon <eric2.monzon@gmail.com>
2024-07-09 22:34:29 +0800

    modifed dataset script

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,6 +1,6 @@
 import torch
 from typing import Tuple
-from torchvision.transforms import v2
+from torchvision import transforms
 from torchvision.datasets import CIFAR10
 
 def get_dataset(data_dir: str) -> Tuple[CIFAR10]:
@@ -22,22 +22,20 @@ def get_dataset(data_dir: str) -> Tuple[CIFAR10]:
         The test set of CIFAR10.
     """
 
-    train_transforms = v2.Compose([
-        v2.RandomCrop(32, padding=4),
-        v2.RandomGrayscale(p=0.2),
-        v2.RandomHorizontalFlip(p=0.2),
-        v2.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
-        v2.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3)),
-        v2.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5.0)),
-        v2.ToImage(),
-        v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    train_transforms = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.RandomHorizontalFlip(p=0.2),
+        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+        transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3)),
+        transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5.0)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
-    val_transforms = v2.Compose([
-        v2.ToImage(),
-        v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    val_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
     train_dataset = CIFAR10(root=data_dir, train=True, download=True, transform=train_transforms)
